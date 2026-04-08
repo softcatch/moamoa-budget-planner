bd119b1ae6bf2ffe377dd438c03445924ffd7514
junsoo0719 <junsoo0719@gmail.com>
2026-04-08 19:32:04 +0900

    [#19] FontAwesome, VCalendar 라이브러리 추가

--- front/src/main.js
+++ front/src/main.js
@@ -1,13 +1,28 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import './assets/style.css';
+import VCalendar from 'v-calendar';
+import 'v-calendar/style.css';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faChevronRight,
+  faCalendarDays,
+  faHouse,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import App from './App.vue';
 import router from './router';
+
+library.add(faChevronRight, faCalendarDays, faHouse);
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VCalendar, {});
+
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app');
